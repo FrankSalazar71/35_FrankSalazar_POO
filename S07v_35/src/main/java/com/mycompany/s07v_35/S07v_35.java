f9417eff3bbdf0a63a6303ae9935d2d3b9aa2627
FrankSalazar71 <140365851+FrankSalazar71@users.noreply.github.com>
2023-10-03 10:04:45 -0500

    NUEVA VERSION

---
+++ S07v_35/src/main/java/com/mycompany/s07v_35/S07v_35.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.s07v_35;
+
+/**
+ *
+ * @author Lab25
+ */
+public class S07v_35 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
